2cec102b334f38f78379108a3e7af307ce47cb6b
julia85310 <juliarueda85310@gmail.com>
2024-11-15 12:11:16 +0100

    upd imagenes arregladas

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,21 @@
 import Card from './Card';
-import image1 from '../images/zanahoria.jpg';
-import image2 from '../images/patata.jpg';
-import image3 from '../images/pimiento.jpg';
 
 export default function App() {
   const cardsData = [
     {
       titulo: 'Zanahoria',
       descripcion: 'Planta herbácea umbelífera, con flores blancas y purpúrea la central de la umbela, con fruto seco y comprimido y raíz fusiforme, de unos 20 centímetro(s) de largo, amarilla o rojiza, jugosa y comestible.',
-      imagen: image1
+      imagen: 'zanahoria.jpg'
     },
     {
       titulo: 'Papa',
       descripcion: 'Sumo pontífice romano, vicario de Cristo, sucesor de san Pedro en el gobierno universal de la Iglesia católica, de la cual es cabeza visible, y padre espiritual de todos los fieles. xd',
-      imagen: image2
+      imagen: 'patata.jpg'
     },
     {
       titulo: 'Pimiento',
       descripcion: 'Planta herbácea anual, de origen americano, de la familia de las solanáceas, con fruto en baya hueca, de color verde, rojo o amarillo y forma más o menos cónica.',
-      imagen: image3
+      imagen: 'pimiento.jpg'
     },
   ];
 
